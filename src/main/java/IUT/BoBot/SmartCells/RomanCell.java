91e9f5dc0d2776c4e6a1dba55a56064be831a212
Alain DRILLON <contact@drillon-ala.in>
2014-02-26 09:54:07 +0100

    Finalized support for roman numerals

--- src/main/java/IUT/BoBot/SmartCells/RomanCell.java
+++ src/main/java/IUT/BoBot/SmartCells/RomanCell.java
@@ -29,11 +29,22 @@ public class RomanCell implements SmartCell{
 	private int toArab(String roman) {
 		int arab = 0;
 		
-		for(int c = 0; c < roman.length(); ++c) {
-			arab += numerals.get(roman.charAt(c));
+		for(int c = 0; c < roman.length(); ++c) {	
+			if(c < roman.length() - 1 && directlyAbove(roman.charAt(c), roman.charAt(c+1))) {
+				arab += numerals.get(roman.charAt(c+1));
+				arab -= numerals.get(roman.charAt(c));
+				
+				++c;
+			} else {
+				arab += numerals.get(roman.charAt(c));
+			}
 		}
 		
 		return arab;
+	}
+
+	private boolean directlyAbove(char c1, char c2) {
+		return (numerals.get(c2)/numerals.get(c1) == 5) || (numerals.get(c2)/numerals.get(c1) == 10);
 	}
 
 }
--- src/test/java/IUT/BoBot/SmartCells/RomanCellTest.java
+++ src/test/java/IUT/BoBot/SmartCells/RomanCellTest.java
@@ -9,10 +9,25 @@ public class RomanCellTest {
 	RomanCell cell = new RomanCell();
 
 	@Test
-	public void testSingleNumbers() {
+	public void testSingleRomanNumbers() {
 		for(final Character numeral: RomanCell.numerals.keySet()) {
 			assertEquals(Integer.toString(RomanCell.numerals.get(numeral)), cell.ask(numeral + " en nombres arabes"));
 		}
+	}
+	
+	@Test
+	public void testMMXIV() {
+		assertEquals(Integer.toString(2014), cell.ask("MMXIV en nombres arabes"));
+	}
+	
+	@Test
+	public void testMIM() {
+		assertEquals(Integer.toString(2001), cell.ask("MIM en nombres arabes"));
+	}
+	
+	@Test
+	public void testVX() {
+		assertEquals(Integer.toString(15), cell.ask("VX en nombres arabes"));
 	}
 
 }
